f4be6b8deabf204fea10370ae8247d23434f5d64
giriraj-vyas <60027227+giriraj-vyas@users.noreply.github.com>
2020-04-07 14:04:51 +0530

    Add files via upload

---
+++ C_ASCII.C
@@ -0,0 +1,13 @@
+  #include"stdio.h"
+  #include"conio.h"
+  void main()
+  {
+  clrscr();
+  printf("\n\t\t\tconverd character to ascii value learn\n");
+  char c;
+  printf("enter a character :");
+  scanf("%c",&c);
+  printf("\n ascii value of %c =%d",c,c);
+  printf("\n\t\t\tcoding is fun !\n\n\n");
+  getch();
+  }
